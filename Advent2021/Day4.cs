bb8e80610f8716c362d17cf7d7fe3bcb720f34d8
Roger Hoem-Martinsen <roger.h.martinsen@gmail.com>
2021-12-04 16:10:21 +0100

    day 4

---
+++ Advent2021/Day4.cs
@@ -0,0 +1,144 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.IO;
+using System.Linq;
+
+namespace Advent2021
+{
+    public record Row(string Raw)
+    {
+        public IImmutableList<int> Values =>
+            Raw.Split(" ").Where(v => !string.IsNullOrEmpty(v)).Select(int.Parse).ToImmutableList();
+
+        public override string ToString()
+        {
+            return string.Join(", ",Values);
+        }
+
+        public bool AllOK(IImmutableList<int> val) => Values.All(val.Contains);
+        public int Unmarked(IImmutableList<int> val) => Values.Where(v => !val.Contains(v)).Sum();
+
+    }
+
+    public record Column(IImmutableList<string> Raw, int ColumnIdx)
+    {
+        public IImmutableList<int> Values => Raw.Select(raw => raw.Split(" ").Where(v => !string.IsNullOrEmpty(v)).Select(int.Parse).ToImmutableList()[ColumnIdx]).ToImmutableList();
+        
+        public override string ToString()
+        {  
+            return string.Join(", ",Values);
+        }
+        
+        public bool AllOK(IImmutableList<int> val) => Values.All(val.Contains);
+    }
+    
+    public record Board(IImmutableList<string> Raw)
+    {
+        public IImmutableList<Row> Rows => Raw.Take(5).Select(v => new Row(v)).ToImmutableList();
+        public IImmutableList<Column> Columns => Raw.Take(5).Select((_, i) => new Column(Raw, i)).ToImmutableList();
+        public override string ToString()
+        {
+            return "Rows:\n" + string.Join("\n", Rows) + "\nColumns\n" + string.Join("\n", Columns);
+        }
+
+        public bool AllOK(IImmutableList<int> val) => Rows.Any(r => r.AllOK(val)) || Columns.Any(c => c.AllOK(val));
+        public int Unmarked(IImmutableList<int> val) => Rows.Select(r => r.Unmarked(val)).Sum();
+    }
+    
+    public class Day4
+    {
+        public void E1()
+        { 
+            var lines = File.ReadAllLines(Path.Join("Files", "day4.txt"));
+            var numbers = lines [0].Split(",").Select(int.Parse).ToImmutableList();
+            var group = new List<string>();
+            var boards = new List<Board>();
+            foreach (var line in lines .Skip(2))
+            {
+                if (string.IsNullOrEmpty(line))
+                {
+                    boards.Add(new Board(group.ToImmutableList()));
+                    group = new List<string>();
+                }
+                else
+                {
+                    group.Add(line);
+                }
+            }
+            boards.Add(new Board(group.ToImmutableList()));
+            
+            var acc = new List<int>();
+            var nums = numbers.TakeWhile(n =>
+            {
+                acc.Add(n);
+                return !boards.Any(b => b.AllOK(acc.ToImmutableList()));
+            }).ToImmutableList();
+            
+            Console.WriteLine(string.Join(", ", acc));
+            
+            var board = boards.First(b => b.AllOK(acc.ToImmutableList()));
+           // Console.WriteLine(board);
+
+            var unmarked = board.Unmarked(acc.ToImmutableList());
+            Console.WriteLine("unmarked " +  unmarked);
+
+            var lastNum = acc.ToImmutableList().Last();
+            Console.WriteLine("lastNum " +  lastNum);
+
+            var result = lastNum * unmarked;
+            Console.WriteLine("Result " +  result);
+        }
+        
+        public void E2()
+        { 
+            var lines = File.ReadAllLines(Path.Join("Files", "day4.txt"));
+            var numbers = lines [0].Split(",").Select(int.Parse).ToImmutableList();
+            var group = new List<string>();
+            var boards = new List<Board>();
+            foreach (var line in lines .Skip(2))
+            {
+                if (string.IsNullOrEmpty(line))
+                {
+                    boards.Add(new Board(group.ToImmutableList()));
+                    group = new List<string>();
+                }
+                else
+                {
+                    group.Add(line);
+                }
+            }
+            boards.Add(new Board(group.ToImmutableList()));
+
+            int last;
+            var list = numbers.ToImmutableList();
+            while (true)
+            {
+                var before = boards.All(b => b.AllOK(list.ToImmutableList()));
+                last = list.TakeLast(1).ToImmutableList()[0];
+                list = list.SkipLast(1).ToImmutableList();
+                var after = boards.All(b => b.AllOK(list.ToImmutableList()));
+                if (after == false)
+                {
+                    break;
+                }
+            }
+            Console.WriteLine(string.Join(", ", last));
+            
+            var board = boards.First(b => !b.AllOK(list.ToImmutableList()));
+            Console.WriteLine(board);
+
+            var l = list.ToImmutableList().Add(last);
+
+            var unmarked = board.Unmarked(l.ToImmutableList());
+            Console.WriteLine("unmarked " +  unmarked);
+
+            var lastNum = l.ToImmutableList().Last();
+            Console.WriteLine("lastNum " +  lastNum);
+
+            var result = lastNum * unmarked;
+            Console.WriteLine("Result " +  result)
+            ;
+        }
+    }
+}
--- Advent2021/Program.cs
+++ Advent2021/Program.cs
@@ -9,7 +9,8 @@ namespace Advent2021
             var day1 = new Day1();
             var day2 = new Day2();
             var day3 = new Day3();
-            day3.E2();
+            var day4 = new Day4();
+            day4.E2();
             return 0;
         }
         
