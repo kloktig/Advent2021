5a3c2aee8ef33eddf5c9e495cc02d677407f709a
Roger Hoem-Martinsen <roger.h.martinsen@gmail.com>
2021-12-06 19:13:49 +0100

    day 5 cleanup and add timer

--- Advent2021/Day5.cs
+++ Advent2021/Day5.cs
@@ -3,8 +3,11 @@ using System.Collections;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Collections.Immutable;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Net.Sockets;
+using System.Security.Cryptography;
 using System.Threading.Tasks;
 
 namespace Advent2021
@@ -22,52 +25,11 @@ namespace Advent2021
             }
         }
 
-        record Line
+        record Line(Point P1, Point P2)
         {
-            public Point P1 { get; }
-            public Point P2 { get; }
-
-            public Line(Point p1, Point p2)
-            {
-                P1 = p1;
-                P2 = p2;
-                
-                IsVerticalOrHorizontalLine = p1.X == p2.X || p1.Y == p2.Y;
-                IsDiagonalLine = Math.Abs(p1.X - p2.X) == Math.Abs(p1.Y - p2.Y);
-            }
-            public readonly bool IsVerticalOrHorizontalLine; 
-            public readonly bool IsDiagonalLine; 
-
-            public bool isOnLine(Point p)
-            {
-                if (IsVerticalOrHorizontalLine)
-                {
-                    var xOk = P1.X < P2.X ? p.X >= P1.X && p.X <= P2.X : p.X <= P1.X && p.X >= P2.X;
-                    var yOk = P1.Y < P2.Y ? p.Y >= P1.Y && p.Y <= P2.Y : p.Y <= P1.Y && p.Y >= P2.Y;
-                    return yOk && xOk;
-                }
-                else if(IsDiagonalLine)
-                {
-                    var num = Math.Abs(P1.X - P2.X);
-                    var incX = P1.X < P2.X ? 1 : -1;
-                    var incY = P1.Y < P2.Y ? 1 : -1;
-
-                    foreach (var number in Enumerable.Range(0, num))
-                    {
-                        var point = new Point(P1.X + number * incX, P1.Y + number * incY);
-                        if (p == point)
-                        {
-                            return true;
-                        }
-                    }
-                    return false;
-                }
-                else
-                {
-                    throw new Exception("Should not happen");
-                }
-            }
-
+            public int YIncrement = P1.Y == P2.Y ? 0 : (P1.Y < P2.Y ? 1 : -1);
+            public int XIncrement = P1.X == P2.X ? 0 : (P1.X < P2.X ? 1 : -1);
+            
             public static Line Parse(string str)
             {
                 var spl = str.Split("->");
@@ -76,27 +38,47 @@ namespace Advent2021
                 return new Line(p1, p2);
             }
         }
+        
+        record Board
+        {
+            private readonly Dictionary<Point, int> _points = new();
+            public Board(int h, int w)
+            {
+                for (var x = 0; x < h; x++)
+                {
+                    for (var y = 0; y < w; y++)
+                    {
+                        _points[new Point(x, y)] = 0;
+                    }
+                }
+            }
+            public int Sum() => _points.Count(v => v.Value >= 2);
+
+            public void Add(Line line)
+            {
+                var length = Math.Max(Math.Abs(line.P2.X - line.P1.X), Math.Abs(line.P2.Y - line.P1.Y));
+                foreach (var i in Enumerable.Range(0, length + 1))
+                {
+                    _points[new Point(line.P1.X + line.XIncrement * i , line.P1.Y + line.YIncrement * i)]++;
+                }
+            }
+        }
 
         public void E1()
         { 
             var strings = File.ReadAllLines(Path.Join("Files", "day5.txt"));
             var lines = strings.Select(Line.Parse).ToImmutableList();
-            var points = (from x in Enumerable.Range(0, 1000) from y in Enumerable.Range(0, 1000) select new Point(x, y)).ToList();
+            
+            var watch = Stopwatch.StartNew();
+            
+            var board = new Board(1000, 1000);
 
-            ConcurrentBag<int> countBag = new ConcurrentBag<int>();
-            ConcurrentBag<int> bag = new ConcurrentBag<int>();
-            Parallel.ForEach(points, point =>
+            foreach (var line in lines)
             {
-                bag.Add(1);
-                if (lines.Count(line => line.isOnLine(point)) > 1)
-                    countBag.Add(1);
-                if (point.Y == 0)
-                {
-                    Console.WriteLine($"{bag.Sum()} -> {countBag.Sum()}");
-                }
-            });
-        
-            Console.WriteLine($"Total count: {countBag.Sum()}");
+                board.Add(line);
+            }
+            var val = string.Join(",", board.Sum());
+            Console.WriteLine($"Total count: {val} in {watch.Elapsed}");
         }
     }
 }
