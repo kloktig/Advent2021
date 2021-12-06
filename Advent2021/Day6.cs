f278478fd7964d869c636a3d4f8e183fc6d46b1a
Roger Hoem-Martinsen <roger.h.martinsen@gmail.com>
2021-12-06 11:51:09 +0100

    day 6

---
+++ Advent2021/Day6.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Advent2021
+{
+    public class Day6
+    {
+
+        public void E1()
+        {
+            var state = "3,5,2,5,4,3,2,2,3,5,2,3,2,2,2,2,3,5,3,5,5,2,2,3,4,2,3,5,5,3,3,5,2,4,5,4,3,5,3,2,5,4,1,1,1,5,1,4,1,4,3,5,2,3,2,2,2,5,2,1,2,2,2,2,3,4,5,2,5,4,1,3,1,5,5,5,3,5,3,1,5,4,2,5,3,3,5,5,5,3,2,2,1,1,3,2,1,2,2,4,3,4,1,3,4,1,2,2,4,1,3,1,4,3,3,1,2,3,1,3,4,1,1,2,5,1,2,1,2,4,1,3,2,1,1,2,4,3,5,1,3,2,1,3,2,3,4,5,5,4,1,3,4,1,2,3,5,2,3,5,2,1,1,5,5,4,4,4,5,3,3,2,5,4,4,1,5,1,5,5,5,2,2,1,2,4,5,1,2,1,4,5,4,2,4,3,2,5,2,2,1,4,3,5,4,2,1,1,5,1,4,5,1,2,5,5,1,4,1,1,4,5,2,5,3,1,4,5,2,1,3,1,3,3,5,5,1,4,1,3,2,2,3,5,4,3,2,5,1,1,1,2,2,5,3,4,2,1,3,2,5,3,2,2,3,5,2,1,4,5,4,4,5,5,3,3,5,4,5,5,4,3,5,3,5,3,1,3,2,2,1,4,4,5,2,2,4,2,1,4".Split(",").Select(int.Parse).ToImmutableList();
+            var bins = Enumerable.Range(0, 9).Select(n => Convert.ToInt64(state.Count(l => l == n))).ToArray();
+            var numNew = 0L;
+            var watch = Stopwatch.StartNew();
+            foreach (var day in Enumerable.Range(1,256))
+            {
+                for (var i = 0; i <= 8; i++)
+                {
+                    switch (i)
+                    {
+                        case 6:
+                            bins[6] = bins[i + 1] + numNew;
+                            break;
+                        case 8:
+                            bins[8] = numNew;
+                            break;
+                        default:
+                            bins[i] = bins[i + 1];
+                            break;
+                    }
+                }
+                numNew = bins[0];
+            }
+            watch.Stop();
+            Console.WriteLine($"Sum " + bins.Sum() + " After: " + watch.Elapsed );
+        }
+    }
+}
--- Advent2021/Program.cs
+++ Advent2021/Program.cs
@@ -6,12 +6,8 @@ namespace Advent2021
     {
         static int Main(string[] args)
         {
-            var day1 = new Day1();
-            var day2 = new Day2();
-            var day3 = new Day3();
-            var day4 = new Day4();
-            var day5 = new Day5();
-            day5.E1();
+            var day = new Day6();
+            day.E1();
             return 0;
         }
         
