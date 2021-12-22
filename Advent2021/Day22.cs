010296b7fa50485788abe6f089d71f00c52fb3a9
Roger Hoem-Martinsen <roger.h.martinsen@gmail.com>
2021-12-22 18:48:17 +0100

    wip

--- Advent2021/Day22.cs
+++ Advent2021/Day22.cs
@@ -1,8 +1,6 @@
 using System;
-using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Collections.Immutable;
-using System.Diagnostics;
 using System.IO;
 using System.Linq;
 
@@ -11,39 +9,88 @@ namespace Advent2021
     public class Day22
     {
         record Cube(int X, int Y, int Z);
-        class Cubes : HashSet<Cube>
-        {
-            public bool On { get; private set; }
-            
-            static HashSet<Cube> Create(string line)
-            {
-                Cubes cubes = new Cubes();
-                string[] spl = line.Substring(3).Split(",");
-                var xR = spl[0].Substring(2).Split("..").Select(int.Parse);
-                for (int  = 10; x <= 12; x++)
-                {
-                    for (int y = 10; y <= 12; y++)
-                    {
-                        for (int z = 10; z <= 12; z++)
-                        {
-                            cubes.Add(new Cube(x, y, z));
-                        }
-                    }
-                }
 
-                cubes.On = line[2] == 'f';
-                return cubes;
+        enum CubeType
+        {
+            On,
+            Off,
+            Outside
+        }
+
+        class CubeSet : HashSet<Cube>
+        {
+            public CubeType CubeType { get; private set; }
+
+            public static CubeSet Create(string line)
+            {
+                CubeSet cubeSet = new CubeSet();
+                string[] spl = line.Replace("on", "").Replace("off", "").TrimStart().Split(",");
+                var xR = spl[0].Substring(2).Split("..").Select(int.Parse).ToArray();
+                var yR = spl[1].Substring(2).Split("..").Select(int.Parse).ToArray();
+                var zR = spl[2].Substring(2).Split("..").Select(int.Parse).ToArray();
+
+               // if (CheckIfInvalid(xR)) return new CubeSet {CubeType = CubeType.Outside};
+               // if (CheckIfInvalid(yR)) return new CubeSet {CubeType = CubeType.Outside};
+               // if (CheckIfInvalid(zR)) return new CubeSet {CubeType = CubeType.Outside};
+
+              var xMin = xR[0]; // Math.Max(-50, xR[0]);
+              var yMin = yR[0]; // Math.Max(-50, yR[0]);
+              var zMin = zR[0]; // Math.Max(-50, zR[0]);
+
+              var xMax = xR[1]; //Math.Min(50, xR[1]);
+              var yMax = yR[1]; // Math.Min(50, yR[1]);
+              var zMax = zR[1]; //Math.Min(50, zR[1]);
+
+              for (int x = xMin; x <= xMax; x++)
+              {
+                  for (int y = yMin; y <= yMax; y++)
+                  {
+                      for (int z = zMin; z <= zMax; z++)
+                      {
+                          cubeSet.Add(new Cube(x, y, z));
+                      }
+                  }
+              }
+
+                cubeSet.CubeType = line[2] == 'f' ? CubeType.Off : CubeType.On;
+                Console.WriteLine($"Done with: {line}");
+                return cubeSet;
+            }
+
+            private static bool CheckIfInvalid(int[] range)
+            {
+                return range[1] < -50 || range[0] > 50;
+            }
+
+            public override string ToString()
+            {
+                return $"{CubeType}\n{this.ToStr("\n")}";
             }
         };
 
         private HashSet<Cube> cubes = new();
+
         public void Do()
         {
-            var lines  = File.ReadAllLines(Path.Join("Files", "day22_test.txt")).ToImmutableList();
-            
-        //    Console.WriteLine(lines.ToStr("\n"));
-           
-            Console.WriteLine(cubes.Count);
+            var allCubes = File.ReadAllLines(Path.Join("Files", "day22_test3.txt")).Select(CubeSet.Create)
+                .ToImmutableList();
+
+            var total = new HashSet<Cube>();
+            foreach (var cubes in allCubes)
+            {
+                //   Console.WriteLine(cubes.ToStr());
+                if (cubes.CubeType == CubeType.On)
+                {
+                    total.UnionWith(cubes);
+                }
+                else if (cubes.CubeType == CubeType.Off)
+                {
+                    total.ExceptWith(cubes);
+                }
+                Console.WriteLine("Count:" + total.Count);
+            }
+            Console.WriteLine("Count:" + total.Count);
+
         }
     }
 }
